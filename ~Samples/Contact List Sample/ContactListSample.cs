d6ae1726f61253e04f3d7fa5683d2b63a81f6c82
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-10 21:39:23 +0530

    Add ScrollRext, LayoutGroup, BGImage properties to ListView for more control

--- ~Samples/Contact List Sample/ContactListSample.cs
+++ ~Samples/Contact List Sample/ContactListSample.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using Adrenak.UPF.Examples;
+using UnityEngine.UI;
 
 public class ContactListSample : MonoBehaviour {
+    public Text message;
     public ContactListView listView;
     public List<ContactListItemViewModel> contacts;
 
     void Start() {
+        listView.LayoutGroup.spacing = 5;
+        listView.BGImage.color = Color.black;
+
         listView.ItemsSource = contacts;
         listView.Clicked += obj => {
-            Debug.Log((obj as ContactListItemView).Model.Name);
+            message.text = (obj as ContactListItemView).Model.Name;
         };
     }
 }
