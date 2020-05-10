b6ec9783ba05e4cc0b860d099bc21affe5ae9a0d
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-10 23:47:32 +0530

    feat: Add pull to refresh to list view

---
+++ ListView/ListRefreshIndicator.cs
@@ -0,0 +1,6 @@
+﻿namespace Adrenak.UPF {
+    public abstract class ListRefreshIndicator : View {
+        public abstract void SetValue(float value);
+        public abstract void SetRefreshing(bool state);
+    }
+}
--- ~Samples/Contact List Sample/ContactListSample.cs
+++ ~Samples/Contact List Sample/ContactListSample.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using Adrenak.UPF.Examples;
 using UnityEngine.UI;
@@ -7,6 +8,7 @@ public class ContactListSample : MonoBehaviour {
     public Text message;
     public ContactListView listView;
     public List<ContactListItemViewModel> contacts;
+    public ContactListItemViewModel extraContact;
 
     void Start() {
         listView.LayoutGroup.spacing = 5;
@@ -15,6 +17,14 @@ public class ContactListSample : MonoBehaviour {
         listView.ItemsSource = contacts;
         listView.Clicked += obj => {
             message.text = (obj as ContactListItemView).Model.Name;
+        };
+        
+        listView.OnPullToRefresh += async () => {
+            await Task.Delay(2000);
+            var items = listView.ItemsSource;
+            items.Add(extraContact);
+            listView.ItemsSource = items;
+            listView.StopRefresh();
         };
     }
 }
