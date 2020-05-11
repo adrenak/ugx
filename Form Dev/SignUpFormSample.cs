fd3d6e3751afbd6fe2e180982bbf7b29a6b08a89
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-11 22:49:25 +0530

    Add basic signup page sample for forms

---
+++ Form Dev/SignUpFormSample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Adrenak.UPF.Examples {
+    public class SignUpFormSample : MonoBehaviour {
+        public SignUpFormView form;
+        public Text message;
+
+        void Start() {
+            form.BindingContext.OnSubmit += (sender, args) => {
+                if (!form.BindingContext.AgreeWithTNC)
+                    message.text = "Please read to T&C";
+                else
+                    message.text = $"Send a sign up request for credentials {form.BindingContext.Email} + {form.BindingContext.Password}";
+            };
+
+            form.BindingContext.OnCancel += (sender, args) =>
+                message.text = "User cancelled sign up. Maybe first ask if they're sure?";
+        }
+    }
+}
---
+++ Form Dev/SignUpFormView.cs
@@ -0,0 +1,14 @@
+﻿using UnityWeld.Binding;
+
+namespace Adrenak.UPF.Examples {
+    [Binding]
+    public class SignUpFormView : View<SignUpFormViewModel> {
+        public void Submit() {
+            BindingContext.Submit();
+        }
+
+        public void Cancel() {
+            BindingContext.Cancel();
+        }
+    }
+}
