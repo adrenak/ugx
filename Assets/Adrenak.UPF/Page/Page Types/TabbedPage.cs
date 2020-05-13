fb96ba2b2c5afd9f685bd17b0bff751ba69aee35
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-13 20:10:52 +0530

    Add default TextListView implementation of ListView Add Alert and Confirmation popups

--- Assets/Adrenak.UPF/Page/Page Types/TabbedPage.cs
+++ Assets/Adrenak.UPF/Page/Page Types/TabbedPage.cs
@@ -5,13 +5,15 @@ using NaughtyAttributes;
 
 namespace Adrenak.UPF {
     public class TabbedPage : Page {
+#pragma warning disable 0649
         [Header("Tabbed Page")]
         [ReadOnly] [SerializeField] Page current;
-
+        
         [SerializeField] int startPageIndex;
         
         [ReorderableList] [SerializeField] List<Page> children;
         public List<Page> Children => children;
+#pragma warning restore 0649
 
         void Start() {
             OpenByIndex(startPageIndex);
---
+++ Assets/Adrenak.UPF/Popups/OptionsPopupItemView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Adrenak.UPF {
+    public class OptionsPopupItem : ViewModel {
+        public event EventHandler OnClick;
+
+        [SerializeField] string label;
+        public string Label {
+            get => label;
+            set => Set(ref label, value);
+        }
+
+        public void Click() {
+            OnClick?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class OptionsPopupItemView : View<OptionsPopupItem> {
+#pragma warning disable 0649
+        [SerializeField] Text labelDisplay;
+        
+#pragma warning restore 0649
+
+        protected override void InitializeFromContext() {
+            labelDisplay.text = Context.Label;
+        }
+
+        public void Click() {
+            Context.Click();
+        }
+
+        protected override void BindViewToContext() { }
+        protected override void OnPropertyChange(string propertyName) { }
+    }
+}
