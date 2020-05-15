b021ce3af6d648093f36c4ad01234c9f496dba80
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-15 23:14:42 +0530

    Refactor UITweener further

--- Animation/Tweening/ITweener.cs
+++ Animation/Tweening/ITweener.cs
@@ -1,10 +1,12 @@
 ﻿using System;
-
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Adrenak.UPF {
     public interface ITweener {
+        Task TweenPosition(RectTransform rt, Vector3 endValue, PositionTween tween);
         void TweenPosition(RectTransform rt, Vector3 endValue, PositionTween tween, Action onComplete);
+        Task TweenOpacity(CanvasGroup group, float endValue, OpacityTween tween);
         void TweenOpacity(CanvasGroup group, float endValue, OpacityTween tween, Action onComplete);
     }
 }
