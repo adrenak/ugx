53da5f9baa39054ce23634636963a19bedcbc80f
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-15 21:50:25 +0530

    Refactor exit cordinate computing

---
+++ Animation/TweeningExtensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public static class TweeningExtensions {
+    public static float GetLeftExit(this RectTransform rt) {
+        var parentRect = rt.parent.GetComponent<RectTransform>().rect;
+        return -parentRect.width / 2 - rt.rect.width / 2;
+    }
+
+    public static float GetRightExit(this RectTransform rt) {
+        var parentRect = rt.parent.GetComponent<RectTransform>().rect;
+        return parentRect.width / 2 + rt.rect.width / 2;
+    }
+
+    public static float GetTopExit(this RectTransform rt) {
+        var parentRect = rt.parent.GetComponent<RectTransform>().rect;
+        return parentRect.height / 2 + rt.rect.height / 2;
+    }
+
+    public static float GetBottomExit(this RectTransform rt) {
+        var parentRect = rt.parent.GetComponent<RectTransform>().rect;
+        return -parentRect.width / 2 - rt.rect.height / 2;
+    }
+}
