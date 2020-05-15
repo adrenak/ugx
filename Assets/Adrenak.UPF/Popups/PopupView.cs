a4f3273de9b080fe410d4e02123db4686bc0b7fd
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-15 20:33:22 +0530

    Add PopupView base class

---
+++ Assets/Adrenak.UPF/Popups/PopupView.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+namespace Adrenak.UPF {
+    public abstract class PopupView<T> : View<T> where T : Model {
+        [SerializeField] protected UnityEvent onPopupOpen;
+        [SerializeField] protected UnityEvent onPopupClose;
+    }
+}
