cd770892c1a70eeeef58abf7822df5b76a2f290f
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-20 02:07:15 +0530

    ModelGroup working v1

--- Assets/Adrenak.UPF/Samples/ModelGroupTest.cs
+++ Assets/Adrenak.UPF/Samples/ModelGroupTest.cs
@@ -11,15 +11,18 @@ public class ModelGroupTest : MonoBehaviour {
 
     // Start is called before the first frame update
     IEnumerator Start() {
+        /*
+         *  group = new ModelGroup<ContactModel>(
+         *      
+         *  );
+         */
+
+        void onCall(object sender, EventArgs e) => 
+            Debug.Log((sender as ContactModel).Name + " calling");
+
         var group = new ModelGroup<ContactModel>(
-            new List<Action<ContactModel>>{
-                x => x.OnCall += (sender, e) => Debug.Log("Calling " + (sender as ContactModel).Name),
-                x => x.OnDelete += (sender, e) => Debug.Log("Delete " + (sender as ContactModel).Name)
-            },
-            new List<Action<ContactModel>>{
-                x => x.OnCall -= (sender, e) => Debug.Log("Calling " + (sender as ContactModel).Name),
-                x => x.OnDelete -= (sender, e) => Debug.Log("Delete " + (sender as ContactModel).Name)
-            }
+            x => x.OnCall += onCall,
+            x => x.OnCall -= onCall
         );
 
         group.Models.AddRange(models);
@@ -27,9 +30,11 @@ public class ModelGroupTest : MonoBehaviour {
         //models.ForEach(x => x.OnCall += X_OnCall);
         yield return new WaitForSeconds(1);
         models[1].Call();
+        
         var old = models[1];
         yield return new WaitForSeconds(1);
-        models.RemoveAt(1);
+
+        group.Models.RemoveAt(1);
         yield return new WaitForSeconds(1);
         old.Call();
     }
