f0c13c4a072954b0783dbd0dcd59ac4421a49685
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-20 17:08:42 +0530

    Add support for ModelGroup with no event handlers

--- Assets/Adrenak.UPF/Runtime/$Core/ModelGroup.cs
+++ Assets/Adrenak.UPF/Runtime/$Core/ModelGroup.cs
@@ -5,17 +5,17 @@ using System.Collections.Specialized;
 
 namespace Adrenak.UPF {
     public class ModelGroup<T> where T : Model {
-        public ObservableCollection<T> Models { get;  } = new ObservableCollection<T>();
+        public ObservableCollection<T> Models { get; } = new ObservableCollection<T>();
         Action<T>[] sub;
         Action<T>[] unsub;
 
         private void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
-            if (e.NewItems != null)
+            if (e.NewItems != null && sub != null)
                 foreach (T item in e.NewItems)
                     foreach (var s in sub)
                         s(item);
 
-            if (e.OldItems != null)
+            if (e.OldItems != null && unsub != null)
                 foreach (T item in e.OldItems)
                     foreach (var u in unsub)
                         u(item);
@@ -28,11 +28,16 @@ namespace Adrenak.UPF {
             unsub = unsubscriber;
         }
 
-        public ModelGroup(IList<T> models, Action<T>[] subscriber, Action<T>[] unsubscriber) {
+        public ModelGroup(IList<T> models) {
             Models.CollectionChanged += OnCollectionChanged;
+            Models.Clear();
+            Models.AddRange(models);
+        }
 
+        public ModelGroup(IList<T> models, Action<T>[] subscriber, Action<T>[] unsubscriber) {
             sub = subscriber;
             unsub = unsubscriber;
+            Models.CollectionChanged += OnCollectionChanged;
             Models.Clear();
             Models.AddRange(models);
         }
