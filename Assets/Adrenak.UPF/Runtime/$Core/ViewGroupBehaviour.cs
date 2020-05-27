111c9c2f7a5bc0413ce0a57c506ffb06c0d04369
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-28 00:38:58 +0530

    feat: Create ViewGroupBehaviour component

---
+++ Assets/Adrenak.UPF/Runtime/$Core/ViewGroupBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Adrenak.UPF {
+    public class ViewGroupBehaviour<TModel, TView> : MonoBehaviour where TModel : Model where TView : View<TModel> {
+        [SerializeField] ViewGroup<TModel, TView> group;
+        public ViewGroup<TModel, TView> Group => group;
+
+        [SerializeField] List<TModel> initialModels;
+
+        void Start() {
+            group.ModelGroup.Models.Clear();
+            group.ModelGroup.Models.AddRange(initialModels);
+        }
+    }
+}
