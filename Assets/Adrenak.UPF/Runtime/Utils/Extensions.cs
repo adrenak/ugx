b0bb1ddf3c7423074518145f6f004f1f2ef7d7b0
Vatsal Ambastha <adrenak_@outlook.com>
2020-07-14 22:22:51 +0530

    feat: Add extension methods to get rect bounds

--- Assets/Adrenak.UPF/Runtime/Utils/Extensions.cs
+++ Assets/Adrenak.UPF/Runtime/Utils/Extensions.cs
@@ -1,10 +1,52 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 namespace Adrenak.UPF {
     public static class Extensions {
         public static void AddRange<T>(this IList<T> destination, IList<T> source) {
             foreach (var element in source)
                 destination.Add(element);
+        }
+
+        public static float GetLeft(this RectTransform rt) {
+            return rt.position.x - rt.rect.width * rt.lossyScale.x / 2;
+        }
+
+        public static float GetRight(this RectTransform rt) {
+            return rt.position.x + rt.rect.width * rt.lossyScale.x / 2;
+        }
+
+        public static float GetTop(this RectTransform rt) {
+            return rt.position.y + rt.rect.height * rt.lossyScale.y / 2;
+        }
+
+        public static float GetBottom(this RectTransform rt) {
+            return rt.position.y + rt.rect.height * rt.lossyScale.y / 2;
+        }
+
+        public static Vector2 GetTopLeft(this RectTransform rt) {
+            var left = rt.GetLeft();
+            var top = rt.GetTop();
+            return new Vector2(left, top);
+        }
+
+        public static Vector2 GetTopRight(this RectTransform rt) {
+            var right = rt.GetRight();
+            var top = rt.GetTop();
+            return new Vector2(right, top);
+        }
+
+        public static Vector2 GetBottomLeft(this RectTransform rt) {
+            var left = rt.GetLeft();
+            var bottom = rt.GetBottom();
+            return new Vector2(left, bottom);
+        }
+
+        public static Vector2 GetBottomRight(this RectTransform rt) {
+            var right = rt.GetRight();
+            var bottom = rt.GetBottom();
+            return new Vector2(right, bottom);
         }
     }
 }
