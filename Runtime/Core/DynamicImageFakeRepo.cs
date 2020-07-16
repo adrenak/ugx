6fa32681e40b0f233a506b91d10c64e064905a92
Vatsal Ambastha <adrenak_@outlook.com>
2020-07-16 20:47:51 +0530

    feat: Simplify DynamicImage caching
    Cache<T> is now DynamicImageRepository
    StatelssRepo added and set as the default repo in DynamicImage

---
+++ Runtime/Core/DynamicImageFakeRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Adrenak.UPF {
+    /// <summary>
+    /// This isn't a cache, it just 
+    /// </summary>
+    public class DynamicImageFakeRepo : DynamicImageRepository {
+        public override Task Init(object obj = null) {
+            return Task.CompletedTask;
+        }
+
+        public override void Free(string location, DynamicImage.Compression compression, DynamicImage instance, Action onSuccess, Action<Exception> onFailure) {
+            // DynamicImageFaceCache does not free instances as it never stores them in the first place
+        }
+
+        public override Task Free(string location, DynamicImage.Compression compression, DynamicImage instance) {
+            return Task.CompletedTask;
+        }
+
+        public override void Get(string location, DynamicImage.Compression compression, DynamicImage instance, Action<Texture2D> onSuccess, Action<Exception> onFailure) {
+            DownloadSprite(location, compression, onSuccess, onFailure);
+        }
+
+        public override Task<Texture2D> Get(string location, DynamicImage.Compression compression, DynamicImage instance) {
+            var source = new TaskCompletionSource<Texture2D>();
+            Get(location, compression, instance,
+                result => source.SetResult(result),
+                exception => source.SetException(exception)
+            );
+            return source.Task;
+        }
+    }
+}
