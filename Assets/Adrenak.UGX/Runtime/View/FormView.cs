e91d20137b6faf56a6f13c0e42e12cc8d1afb687
Vatsal Ambastha <adrenak_@outlook.com>
2020-09-14 17:59:39 +0530

    fix: Unmark ViewEvent as Serializable

--- Assets/Adrenak.UGX/Runtime/View/FormView.cs
+++ Assets/Adrenak.UGX/Runtime/View/FormView.cs
@@ -1,14 +1,9 @@
-﻿using System;
-
-namespace Adrenak.UGX {
+﻿namespace Adrenak.UGX {
     public abstract class FormView<T> : View<T> where T : ViewModel {
         public ViewEvent onSubmit = new ViewEvent();
         public ViewEvent onCancel = new ViewEvent();
 
-        public void Submit() =>
-            onSubmit.Invoke(this);
-
-        public void Cancel() =>
-            onCancel.Invoke(this);
+        public void Submit() => onSubmit.Invoke(this);
+        public void Cancel() => onCancel.Invoke(this);
     }
 }
