fd9c75e2c45a58fb2099547a998d3304960d649f
Vatsal Ambastha <adrenak_@outlook.com>
2020-09-14 17:59:39 +0530

    fix: Unmark ViewEvent as Serializable

--- Runtime/$Misc/ViewEvent.cs
+++ Runtime/$Misc/ViewEvent.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
 namespace Adrenak.UGX {
-    [Serializable]
     public class ViewEvent<T> {
         public event EventHandler<T> Handler;
         public void Invoke(object sender, T t) => Handler?.Invoke(sender, t);
     }
 
-    [Serializable]
     public class ViewEvent {
         public event EventHandler Handler;
         public void Invoke(object sender) => Handler?.Invoke(sender, EventArgs.Empty);
