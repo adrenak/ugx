a7764fe1599463de19ac9c71a11da07e4fa9e2a0
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2020-11-06 14:53:26 +0530

    feat: Make UGXInitializer MonoBehaviour based

---
+++ Assets/Adrenak.UGX/Runtime/UGXInitializer.cs
@@ -0,0 +1,26 @@
+﻿using Adrenak.Unex;
+
+using UnityEngine;
+
+namespace Adrenak.UGX {
+    public class UGXInitializer : MonoBehaviour {
+        static UGXInitializer instance;
+
+        void Awake() {
+            Init();
+        }
+
+        static void EnsureInstance() {
+            if (instance == null)
+                instance = FindObjectOfType<UGXInitializer>();
+            if (instance == null)
+                instance = new GameObject("UGX Initializer").AddComponent<UGXInitializer>();
+        }
+
+        public static void Init() {
+            EnsureInstance();
+            Dispatcher.Init();
+            Runnable.Init();
+        }
+    }
+}
