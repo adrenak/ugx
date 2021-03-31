08ba18c889abbb669327082a18dcc449eeb2303f
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2021-03-31 14:21:01 +0530

    refactor: Transitions refactored. Resolve #8, Resolve #16, Resolve #17
    - ITransitioner is now ITransitionDriver
    - TransitionerBase introduced with ambient context TransitionDriver
    - Naming made clearer

---
+++ Assets/Adrenak.UGX/Runtime/Transitions/Transitioners/TransitionerBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Adrenak.UGX {
+    public class TransitionerBase : UGXBehaviour {
+        static ITransitionDriver driver;
+
+        public static bool DriverLocked => driver != null;
+        public static ITransitionDriver Driver {
+            get {
+                if (driver == null)
+                    driver = new SurgeTransitionDriver();
+                return driver;
+            }
+            set {
+                if (driver != null)
+                    Debug.LogError("BaseTransitioner.Driver can only be set once and before any get calls");
+
+                if (value == null)
+                    Debug.LogError("BaseTransitioner.Driver cannot set Driver to null!");
+
+                driver = value;
+            }
+        }
+    }
+}
