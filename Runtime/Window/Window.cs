16b850724a108ae57fca36c0638f7d6bbccbadd0
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2021-04-24 01:48:26 +0530

    fix: Some inspector serialization.

--- Runtime/Window/Window.cs
+++ Runtime/Window/Window.cs
@@ -6,32 +6,36 @@ using NaughtyAttributes;
 
 namespace Adrenak.UGX {
     public class Window : UGXBehaviour {
-        [ReadOnly] public WindowStatus status;
+        [ReadOnly] [SerializeField] WindowStatus status;
+        public WindowStatus Status {
+            get => status;
+            private set => status = value; 
+        }
+
         public bool autoPopOnBack = true;
 
-        [BoxGroup("Window Events")] [SerializeField] bool showEvents;
-        [BoxGroup("Window Events")] [ShowIf("showEvents")] public UnityEvent onWindowStartOpening;
-        [BoxGroup("Window Events")] [ShowIf("showEvents")] public UnityEvent onWindowDoneOpening;
-        [BoxGroup("Window Events")] [ShowIf("showEvents")] public UnityEvent onWindowStartClosing;
-        [BoxGroup("Window Events")] [ShowIf("showEvents")] public UnityEvent onWindowDoneClosing;
+        public UnityEvent onWindowStartOpening;
+        public UnityEvent onWindowDoneOpening;
+        public UnityEvent onWindowStartClosing;
+        public UnityEvent onWindowDoneClosing;
 
         [Button]
         async public void OpenWindow() => await OpenWindowAsync();
 
         async public UniTask OpenWindowAsync() {
             await UniTask.SwitchToMainThread();
-            if (status == WindowStatus.Opened || status == WindowStatus.Opening) return;
+            if (Status == WindowStatus.Opened || Status == WindowStatus.Opening) return;
 
             onWindowStartOpening?.Invoke();
 
-            status = WindowStatus.Opening;
+            Status = WindowStatus.Opening;
             var transitions = transitioners.Where(x => x.enabled)
                 .Select(x => x.TransitionInAsync())
                 .ToList();
 
             await UniTask.WhenAll(transitions);
             await UniTask.SwitchToMainThread();
-            status = WindowStatus.Opened;
+            Status = WindowStatus.Opened;
 
             WindowOpened();
             onWindowDoneOpening?.Invoke();
@@ -42,18 +46,18 @@ namespace Adrenak.UGX {
 
         async public UniTask CloseWindowAsync() {
             await UniTask.SwitchToMainThread();
-            if (status == WindowStatus.Closed || status == WindowStatus.Closing) return;
+            if (Status == WindowStatus.Closed || Status == WindowStatus.Closing) return;
 
             onWindowStartClosing?.Invoke();
 
-            status = WindowStatus.Closing;
+            Status = WindowStatus.Closing;
             var transitions = transitioners.Where(x => x.enabled)
                 .Select(x => x.TransitionOutAsync())
                 .ToList();
 
             await UniTask.WhenAll(transitions);
             await UniTask.SwitchToMainThread();
-            status = WindowStatus.Closed;
+            Status = WindowStatus.Closed;
 
             WindowClosed();
             onWindowDoneClosing?.Invoke();
